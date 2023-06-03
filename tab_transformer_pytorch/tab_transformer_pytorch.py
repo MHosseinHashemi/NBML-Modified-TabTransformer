da4921eb57b7bc4ebe297ff8a87f88a0b526d77a
Mohammad Hossein Hashemi <90381570+MHosseinHashemi@users.noreply.github.com>
2023-06-03 23:33:59 +0330

    Update tab_transformer_pytorch.py
    Modified specific sections to make it compatible with BrTC data which is fully continuous.

--- tab_transformer_pytorch/tab_transformer_pytorch.py
+++ tab_transformer_pytorch/tab_transformer_pytorch.py
@@ -141,7 +141,7 @@ class TabTransformer(nn.Module):
     def __init__(
         self,
         *,
-        categories,
+        # categories,
         num_continuous,
         dim,
         depth,
@@ -156,25 +156,25 @@ class TabTransformer(nn.Module):
         ff_dropout = 0.
     ):
         super().__init__()
-        assert all(map(lambda n: n > 0, categories)), 'number of each category must be positive'
-        assert len(categories) + num_continuous > 0, 'input shape must not be null'
+        # assert all(map(lambda n: n > 0, categories)), 'number of each category must be positive'
+        # assert len(categories) + num_continuous > 0, 'input shape must not be null'
 
         # categories related calculations
 
-        self.num_categories = len(categories)
-        self.num_unique_categories = sum(categories)
+        # self.num_categories = len(categories)
+        # self.num_unique_categories = sum(categories)
 
         # create category embeddings table
 
-        self.num_special_tokens = num_special_tokens
-        total_tokens = self.num_unique_categories + num_special_tokens
+        # self.num_special_tokens = num_special_tokens
+        # total_tokens = self.num_unique_categories + num_special_tokens
 
         # for automatically offsetting unique category ids to the correct position in the categories embedding table
 
-        if self.num_unique_categories > 0:
-            categories_offset = F.pad(torch.tensor(list(categories)), (1, 0), value = num_special_tokens)
-            categories_offset = categories_offset.cumsum(dim = -1)[:-1]
-            self.register_buffer('categories_offset', categories_offset)
+        # if self.num_unique_categories > 0:
+        #     categories_offset = F.pad(torch.tensor(list(categories)), (1, 0), value = num_special_tokens)
+        #     categories_offset = categories_offset.cumsum(dim = -1)[:-1]
+        #     self.register_buffer('categories_offset', categories_offset)
 
         # continuous
         self.num_continuous = num_continuous
@@ -190,7 +190,8 @@ class TabTransformer(nn.Module):
         # transformer
 
         self.transformer = Transformer(
-            num_tokens = total_tokens,
+            # num_tokens = total_tokens,
+            num_tokens = num_continuous,
             dim = dim,
             depth = depth,
             heads = heads,
@@ -201,7 +202,8 @@ class TabTransformer(nn.Module):
 
         # mlp to logits
 
-        input_size = (dim * self.num_categories) + num_continuous
+        # input_size = (dim * self.num_categories) + num_continuous
+        input_size = num_continuous
         l = input_size // 8
 
         hidden_dimensions = list(map(lambda t: l * t, mlp_hidden_mults))
@@ -212,7 +214,7 @@ class TabTransformer(nn.Module):
     def forward(self, x_categ, x_cont, return_attn = False):
         xs = []
 
-        assert x_categ.shape[-1] == self.num_categories, f'you must pass in {self.num_categories} values for your categories input'
+        # assert x_categ.shape[-1] == self.num_categories, f'you must pass in {self.num_categories} values for your categories input'
 
         if self.num_unique_categories > 0:
             x_categ += self.categories_offset
