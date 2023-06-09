d94445e4db7b792da7846a1297530926b355a65b
Mohammad Hossein Hashemi <90381570+MHosseinHashemi@users.noreply.github.com>
2023-06-09 18:38:06 +0330

    Update tab_transformer_pytorch.py
    Removed the commented lines

--- tab_transformer_pytorch/tab_transformer_pytorch.py
+++ tab_transformer_pytorch/tab_transformer_pytorch.py
@@ -33,17 +33,11 @@ class PreNorm(nn.Module):
 
 # attention
 
-# class GEGLU(nn.Module):
-#     def forward(self, x):
-#         x, gates = x.chunk(2, dim = -1)
-#         return x * F.gelu(gates)
-
 class FeedForward(nn.Module):
     def __init__(self, dim, mult = 4, dropout = 0.):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(dim, dim * mult * 2),
-#             GEGLU(),
             nn.Sigmoid(),
             nn.Dropout(dropout),
             nn.Linear(dim * mult, dim)
@@ -157,26 +151,6 @@ class TabTransformer(nn.Module):
         ff_dropout = 0.
     ):
         super().__init__()
-        # assert all(map(lambda n: n > 0, categories)), 'number of each category must be positive'
-        # assert len(categories) + num_continuous > 0, 'input shape must not be null'
-
-        # categories related calculations
-
-        # self.num_categories = len(categories)
-        # self.num_unique_categories = sum(categories)
-
-        # create category embeddings table
-
-        # self.num_special_tokens = num_special_tokens
-        # total_tokens = self.num_unique_categories + num_special_tokens
-
-        # for automatically offsetting unique category ids to the correct position in the categories embedding table
-
-        # if self.num_unique_categories > 0:
-        #     categories_offset = F.pad(torch.tensor(list(categories)), (1, 0), value = num_special_tokens)
-        #     categories_offset = categories_offset.cumsum(dim = -1)[:-1]
-        #     self.register_buffer('categories_offset', categories_offset)
-
         # continuous
         self.num_continuous = num_continuous
 
@@ -203,7 +177,6 @@ class TabTransformer(nn.Module):
 
         # mlp to logits
 
-        # input_size = (dim * self.num_categories) + num_continuous
         input_size = num_continuous
         l = input_size // 8
 
@@ -212,19 +185,8 @@ class TabTransformer(nn.Module):
 
         self.mlp = MLP(all_dimensions, act = mlp_act)
 
-#     def forward(self, x_categ, x_cont, return_attn = False):
     def forward(self, x_cont, return_attn = False):
         xs = []
-
-        # assert x_categ.shape[-1] == self.num_categories, f'you must pass in {self.num_categories} values for your categories input'
-
-#         if self.num_unique_categories > 0:
-#             x_categ += self.categories_offset
-
-#             x, attns = self.transformer(x_categ, return_attn = True)
-
-#             flat_categ = x.flatten(1)
-#             xs.append(flat_categ)
 
         assert x_cont.shape[1] == self.num_continuous, f'you must pass in {self.num_continuous} values for your continuous input'
 
